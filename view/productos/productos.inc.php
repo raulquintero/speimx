85f738f0dfc2b4835c9d08d52732dd6c4a241379
Raul Quintero <raul.quintero@live.com>
2016-01-14 23:16:56 -0800

    modificaciones a lista de productos

--- view/productos/productos.inc.php
+++ view/productos/productos.inc.php
@@ -1,13 +1,30 @@
+<div class="hidden-print ">
+					<form action="/index.php" method="get">
+							<table  width=100%  >
+				 			<tr bgcolor=#dddddd>
+				  			<td style="padding:10px">
+
+							<input type=hidden name=data value=productos>
+							<input type=hidden name=filtro value=buscar>
+				  			 &nbsp;&nbsp;Codigo: <input class="input-large" id="textcode" name="code" >
+				  			</td>
+				  		</tr>
+				  		</table>
+				  		</form>
+				  	</div>
+
+
+
 
 <div class="box-content buttons">
-						<a href="/index.php?data=productos&op=producto_form&f=agregar"><button class="btn btn-small btn-primary"><b>+</b> AGREGAR PRODUCTO</button></a>
-						<a href="/index.php?data=empresas&op=empresa_form&f=agregar"><button class="btn btn-small btn-primary"><b>+</b> AGREGAR CATEGORIA</button></a>
-						<a href="/index.php?data=empresas&op=empresa_form&f=agregar"><button class="btn btn-small btn-primary"><b>+</b> AGREGAR SUBCATEGORIA</button></a>
-						<a href="/index.php?data=proveedores&op=proveedor_form&f=agregar"><button class="btn btn-small btn-info"><b>+</b> AGREGAR PROVEEDOR</button></a>
-						
+						<a href="/index.php?data=productos&op=producto_form&f=agregar" class="btn btn-small btn-primary hidden-print"><b>+</b> AGREGAR PRODUCTO</a>
+						<a href="/index.php?data=empresas&op=empresa_form&f=agregar" class="btn btn-small btn-primary hidden-print"><b>+</b> AGREGAR CATEGORIA</a>
+						<a href="/index.php?data=empresas&op=empresa_form&f=agregar" class="btn btn-small btn-primary hidden-print"><b>+</b> AGREGAR SUBCATEGORIA</a>
+						<a href="/index.php?data=proveedores&op=proveedor_form&f=agregar" class="btn btn-small btn-info hidden-print"><b>+</b> AGREGAR PROVEEDOR</a>
+
 </div>
-						
-<div class="box span12 hidden-phone" >
+
+<div class="box span12  " >
 					<div class="box-header" data-original-title>
 						<h2><i class="halflings-icon barcode"></i><span class="break"></span>Productos</h2>
 						<div class="box-icon">
@@ -20,7 +37,7 @@
 						<table class="table table-striped table-bordered bootstrap-datatable datatable">
 						  <thead>
 							  <tr>
-								  <th>ID</th>
+								  <th class="hidden-print">ID</th>
 								  <th>Producto</th>
 								  <th>Subcategoria</th>
 								  <th>PCM</th>
@@ -30,16 +47,32 @@
                                   <th>DESC %</th>
 								  <th>Stock</th>
 								  <th>Proveedor</th>
-								  <th>Acciones</th>
+								  <th class="hidden-print">Acciones</th>
 
 							  </tr>
 						  </thead>
 						  <tbody>
 
 						<?php
+                        if ($_GET['code'])
+                        {
+                        	$query = "SELECT  producto_id from inventariodet
+                            where codigo=".$_GET['code'];
+										list( $prid ) = $database->get_row( $query );
+                         }
 
-						$query = "SELECT producto_id,producto,precio_compra,precio_contado,precio_credito,descuento,precio_promocion,stock,proveedor,subcategoria,codigo FROM producto,proveedor,subcategoria
+
+						$query = "SELECT producto_id,producto,precio_compra,precio_contado,precio_credito,descuento,precio_promocion,stock,proveedor.proveedor_id,proveedor,subcategoria.subcategoria_id,subcategoria,codigo FROM producto,proveedor,subcategoria
 								where producto.proveedor_id=proveedor.proveedor_id AND producto.subcategoria_id=subcategoria.subcategoria_id";
+                                switch ($_GET['filtro'])   {
+                                    case "sub":$filtro=" AND subcategoria.subcategoria_id=".$_GET['sub'];
+                                    break;
+                                    case "proveedor": $filtro= " AND proveedor.proveedor_id=".$_GET['pid'];
+                                    break;
+                                    case "buscar" : if ($prid) $filtro= " AND producto.producto_id=$prid";
+                                    break;
+                                }
+                        $query.=$filtro;
 								$results = $database->get_results( $query );
 						foreach( $results as $row )
 						{
@@ -47,39 +80,48 @@
 							$vlimp=(($row['precio_contado']*1.16)/2);
 						?>
 							<tr>
-								<td class="center"><?php echo $row['codigo']?></td>
-								<td><a href=index.php?data=productos&op=detalles&prid=<?=$row['producto_id']?>><?php echo strtoupper($row['producto'])?></a></td>
-								<td ><?php echo $row['subcategoria']?></td>
+								<td class="center hidden-print"><?php echo $row['codigo']?></td>
+								<td><a class="hidden-print" href="index.php?data=productos&op=detalles&prid=<?php $row['producto_id']?>"><?php echo strtoupper($row['producto'])?></a>
+                                <span class=hidden-desktop><?php echo strtoupper($row['producto'])?></span>
+                                </td>
+								<td><a class="hidden-print" href="index.php?data=productos&filtro=sub&sub=<?php echo $row['subcategoria_id']?>"><?php echo $row['subcategoria']?></a>
+                                <span class=hidden-desktop><?php echo $row['subcategoria']?></span></td>
 								<td style="text-align:right;"><?php echo dinero($row['precio_compra']*1.16)?></td>
-								<td <?php if ($vlimp>$row['precio_compra']) echo "style=\"text-align:right;\">"; else echo "style=\"text-align:right;background:red;color:white;\"> ** ";?>
-									 <?php echo dinero(($row['precio_contado']*1.16)/2)?></td>
+								<?php echo "<td";
+                                  if ($vlimp>$row['precio_compra']) echo " style=\"text-align:right;\">";
+                                        else echo " style=\"text-align:right;background:red;color:white;\"> ** ";
+
+									 echo dinero(($row['precio_contado']*1.16)/2);
+                                     echo "</td>";
+                                     ?>
 								<td style="text-align:right;"><?php echo dinero($row['precio_contado']*1.16)?></td>
 								<td style="text-align:right;"><?php echo dinero($row['precio_credito']*1.16)?></td>
                                 <td style="text-align:right;"><?php echo dinero($row['descuento'])?></td>
 								<td style="text-align:right;"><?php echo $row['stock']?></td>
-								<td class="center"><?php echo $row['proveedor']?></td>
-								<td class="center">
-									<a class="btn btn-success" href="index.php?data=productos&op=detalles&prid=<?php echo $row['producto_id']?>">
+								<td class="center"><a class="hidden-print" href="index.php?data=productos&filtro=proveedor&pid=<?php echo $row['proveedor_id']?>"><?php echo $row['proveedor']?></a>
+                                <span class=hidden-desktop><?php echo $row['proveedor']?></span></td>
+								<td class="center hidden-print">
+									<a class="btn btn-success hidden-print" href="index.php?data=productos&op=detalles&prid=<?php echo $row['producto_id']?>">
 										<i class="halflings-icon white zoom-in"></i>
 									</a>
-									<a href="index.php?data=productos&op=producto_form&f=editar&prid=<?php echo $row['producto_id']?>">
-													<button class="btn btn-primary"><i class="halflings-icon white edit"></i></button></a>
+									<a href="index.php?data=productos&op=producto_form&f=editar&prid=<?php echo $row['producto_id']?>"
+													class="btn btn-primary hidden-print"><i class="halflings-icon white edit"></i></a>
 
-									<a href="index.php?data=clientes&op=subirfoto&cid=<?php echo $producto_id?>">
-											<button class="btn btn-info"><i class="halflings-icon white th-list"></i></button></a><br>
+									<a href="index.php?data=clientes&op=subirfoto&cid=<?php echo $producto_id?>"
+											 class="btn btn-info hidden-print"><i class="halflings-icon white th-list"></i></a><br>
 									<!-- <a class="btn btn-danger" href="#">
-										<i class="halflings-icon white trash"></i> 
+										<i class="halflings-icon white trash"></i>
 									</a> -->
 								</td>
 							</tr>
 
-<?php 
+<?php
 	}
 ?>
 
 
 						  </tbody>
-					  </table>            
+					  </table>
 					</div>
 				</div><!--/span-->
 
@@ -87,7 +129,7 @@
 
 
 
-<div class="box span12 hidden-desktop" >
+<div class="box span12 hidden-desktop hidden-print" >
 					<div class="box-header" data-original-title>
 						<h2><i class="halflings-icon barcode"></i><span class="break"></span>Productos</h2>
 						<div class="box-icon">
@@ -104,16 +146,17 @@
 								  <th>Producto</th>
 								  <th>Subcategoria</th>
 								  <th>Proveedor</th>
-								  <th>Acciones</th>
+								  <th class="hidden-print">Acciones</th>
 
 							  </tr>
-						  </thead>   
+						  </thead>
 						  <tbody>
 
 						<?php
 
-						$query = "SELECT producto.codigo,producto_id,producto,precio_compra,precio_contado,precio_credito,stock,proveedor,subcategoria FROM producto,proveedor,subcategoria 
+						$query = "SELECT producto.codigo,producto_id,producto,precio_compra,precio_contado,precio_credito,stock,proveedor,subcategoria FROM producto,proveedor,subcategoria
 								where producto.proveedor_id=proveedor.proveedor_id AND producto.subcategoria_id=subcategoria.subcategoria_id";
+                        $query.=$filtro;
 								$results = $database->get_results( $query );
 						foreach( $results as $row )
 						{
@@ -122,26 +165,28 @@
 						?>
 							<tr>
 								<td class="center"><?php echo $row['codigo']?></td>
-								<td><a href=index.php?data=productos&op=detalles&prid=<?=$row['producto_id']?>><?php echo strtoupper($row['producto'])?></a><br>
-									PCM: $<?php echo ($row['precio_compra']+($row['precio_credito']*.16))?> PCN: $<?php echo ($row['precio_contado']+($row['precio_contado']*.16))?> PVE: $<?php echo ($row['precio_credito']+($row['precio_credito']*.16))?> </td>
+								<td>
+                                <a class="hidden-print" href=index.php?data=productos&op=detalles&prid=<?=$row['producto_id']?>><?php echo strtoupper($row['producto'])?></a>
+                                <?php echo strtoupper($row['producto'])?>
+                                PCM: $<?php echo ($row['precio_compra']+($row['precio_credito']*.16))?> PCN: $<?php echo ($row['precio_contado']+($row['precio_contado']*.16))?> PVE: $<?php echo ($row['precio_credito']+($row['precio_credito']*.16))?> </td>
 								<td class="center"><?php echo $row['subcategoria']?></td>
 								<td class="center"><?php echo $row['proveedor']?></td>
-								<td class="center">  
-									<a class="btn btn-success" href="index.php?data=productos&op=detalles&prid=<?php echo $row['producto_id']?>">
-										<i class="halflings-icon white zoom-in"></i>  
+								<td class="center hidden-print">
+									<a class="btn btn-success hidden-print" href="index.php?data=productos&op=detalles&prid=<?php echo $row['producto_id']?>">
+										<i class="halflings-icon white zoom-in"></i>
 									</a>
-									<a href="index.php?data=productos&op=producto_form&f=editar&prid=<?php echo $row['producto_id']?>">
-													<button class="btn btn-primary"><i class="halflings-icon white edit"></i></button></a>
+									<a href="index.php?data=productos&op=producto_form&f=editar&prid=<?php echo $row['producto_id']?>"
+													class="btn btn-primary hidden-print"><i class="halflings-icon white edit"></i></a>
 
-									<a href="index.php?data=clientes&op=subirfoto&cid=<?php echo $producto_id?>">
-											<button class="btn btn-info hidden"><i class="halflings-icon white th-list"></i></button></a><br>
+									<a href="index.php?data=clientes&op=subirfoto&cid=<?php echo $producto_id?>"
+											 class="btn btn-info hidden"><i class="halflings-icon white th-list"></i></a><br>
 									<!-- <a class="btn btn-danger" href="#">
-										<i class="halflings-icon white trash"></i> 
+										<i class="halflings-icon white trash"></i>
 									</a> -->
 								</td>
 							</tr>
 
-<?php 
+<?php
 	}
 ?>
 
